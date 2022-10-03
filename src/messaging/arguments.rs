29fbe2641ce2b76c5c37de89066bd8b8c19615fe
Thom Dickson <td3of4@gmail.com>
2022-10-03 16:59:18 -0400

    refactor: move arguments tests into submodule file

--- src/messaging/arguments.rs
+++ src/messaging/arguments.rs
@@ -1,4 +1,7 @@
 // Directive specific abstractions for parsing the byte array argument data
+
+mod tests;
+
 use base64ct::{Base64, Encoding};
 use core::fmt::Debug;
 use serde::{Deserialize, Serialize};
@@ -360,61 +363,6 @@ impl Argument for Dummy {
     }
 
     fn as_any(&self) -> &dyn Any {
-        todo!()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use sha2::{Digest, Sha256};
-
-    use super::*;
-
-    #[test]
-    fn test_argument_version() {
-        assert_eq!(Version(1).to_bin(), vec![1u8]);
-        assert_eq!(Version::from_bin(&[1u8]).unwrap(), Version(1));
-    }
-
-    #[test]
-    fn test_argument_fileid() {
-        let mut h = Sha256::new();
-        let mut a = vec![112, 97, 116, 104, 47, 116, 111, 47, 102, 105, 108, 101];
-        h.update(b"Hello world");
-        let mut b = [0u8; 32];
-        b.copy_from_slice(&h.finalize());
-        a.extend_from_slice(&b);
-        assert_eq!(
-            FileId {
-                path: PathBuf::from("path/to/file"),
-                hash: b
-            }
-            .to_bin(),
-            a
-        );
-        assert_eq!(
-            FileId::from_bin(&a).unwrap(),
-            FileId {
-                path: PathBuf::from("path/to/file"),
-                hash: b
-            }
-        );
-    }
-
-    #[test]
-    fn test_argument_chunkid() {
-        assert_eq!(
-            ChunkId(b"Hello world".to_vec()).to_bin(),
-            vec![72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]
-        );
-        assert_eq!(
-            ChunkId::from_bin(&[72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]).unwrap(),
-            ChunkId(b"Hello world".to_vec())
-        );
-    }
-
-    #[test]
-    fn test_argument_filelist() {
         todo!()
     }
 }
